59eedd61b31a86166ed0a97278bb7a2838ade77a
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-02-22 14:45:29 +0000

    deploy: f7cb35a8c7f3f63b5a7986003d92bf4a67692371

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 
+const copyLinks = document.querySelectorAll('.copy-link');
+const address = document.location.href;
+
+
+function copyToClipboard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+function addAlertCopyText (copyButton) {
+  const span = document.createElement('span');
+  span.classList.add('copy-link-alert');
+  span.textContent = "Ссылка скопирована";
+  copyButton.appendChild(span);
+  setTimeout(() => span.remove(),1000);
+};
+
+if (copyLinks) {
+  copyLinks.forEach(copyLink => {
+    copyLink.addEventListener('click', () => {
+      copyToClipboard(address);
+      addAlertCopyText(copyLink);
+    })
+  });
+}
+
+
+
+
