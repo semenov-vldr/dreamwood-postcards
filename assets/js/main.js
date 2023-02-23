608a4ed681a3c4ad2ebedf4ee352c2212b9eac00
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-02-23 10:18:41 +0000

    deploy: 3651aa48c6f1f84a79387da29cfe3732a8f3ca3c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 const copyLinks = document.querySelectorAll('.copy-link');
-const address = document.location.href;
+const address = window.location.href;
 
 
 function copyToClipboard(str) {
@@ -31,5 +31,26 @@ if (copyLinks) {
 }
 
 
+
+const url_telegram = 'https://t.me/share/url?url=';
+const url_viber = 'viber://forward?text=';
+const url_vk = 'https://vk.com/share.php?url=';
+const url_ok = 'https://connect.ok.ru/offer?url=';
+
+
+const socialList = document.querySelector('.social__list');
+if (socialList) {
+
+  const link_telegram = socialList.querySelector('.social__link--telegram');
+  const link_viber = socialList.querySelector('.social__link--viber');
+  const link_vk = socialList.querySelector('.social__link--vk');
+  const link_ok = socialList.querySelector('.social__link--ok');
+
+  link_telegram.href = `${url_telegram + address}`;
+  link_viber.href = `${url_viber + address}`;
+  link_vk.href = `${url_vk + address}`;
+  link_ok.href = `${url_ok + address}`;
+
+}
 
 
